12004227575ce9c513b2fe5f04c1c1fa531dafc3
brand <mamyrgulzhursunbekova@gmail.com>
2024-12-12 15:19:22 +0600

    Announcement created CRUD

--- src/dao/AnnouncementDao.java
+++ src/dao/AnnouncementDao.java
@@ -1,4 +1,14 @@
 package dao;
 
+import models.Announcement;
+import models.User;
+
+import java.util.List;
+
 public interface AnnouncementDao {
+    Announcement getAnnouncementById(Long id);
+    List<Announcement> getAllAnnouncements();
+    String saveAnnouncement(Announcement announcement);
+    Announcement updateAnnouncement(Long id,Announcement announcement);
+    void deleteAnnouncementById(Long id);
 }
